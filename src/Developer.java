f257d85a848dd5d4f002b84cb3c64e25e40eea08
goktas1946 <goktas1946@gmail.com>
2020-09-15 20:21:05 -0500

    inital commit

---
+++ src/Developer.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Developer extends Employee{
+
+    public Developer(String name, char gender, LocalDate DOfB, String jobTitle, double salary) {
+        super(name, gender, DOfB, jobTitle, salary);
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public final class Tester extends Employee{
+
+    public Tester(String name, char gender, LocalDate DOfB, String jobTitle, double salary) {
+        super(name, gender, DOfB, jobTitle, salary);
+    }
+
+    public void findBug(){
+        System.out.println(name+" found a bug");
+    }
+
+}
